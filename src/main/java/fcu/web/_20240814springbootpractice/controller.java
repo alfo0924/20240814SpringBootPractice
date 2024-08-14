6d344eebc484c0b5e3e0d555eddf14ef9f7611a1
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-14 21:47:37 +0800

    v1.11 SpringBoot initial

--- src/main/java/fcu/web/_20240814springbootpractice/controller.java
+++ src/main/java/fcu/web/_20240814springbootpractice/controller.java
@@ -8,10 +8,24 @@ public class controller {
 
 @RequestMapping("/SpringBootInitPractice")
 public String test(){
-
     System.out.println("Hello from SpringBoot !");
     return "hello from JAVASpringBoot ";
+    }
 
-
+    @RequestMapping("/test1")
+    public String test1(){
+    System.out.println("test1");
+    return "Hello from SpringBoot test1 !";
+    }
+    @RequestMapping("/test2")
+    public String test2(){
+    System.out.println("test2");
+    return"Hello from SpringBoot test2";
+    }
+    @RequestMapping("/test3")
+    public String test3(){
+    System.out.println("test3");
+    return"Hello from SpringBoot test3";
     }
 }
+
